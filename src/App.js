6885d2ab5afa1743a67d68b31aa5c27a72bda1ba
Yudi Santoso <32637242+judjo@users.noreply.github.com>
2019-11-06 11:25:00 +0700

    test
    summit first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to={'/'} className="navbar-brand">React CRUD Example</link>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+      <Link to={'/create'} className="nav-link">Create</Link>
+      </li>
+      <li className="nav-item">
+      <
+      </Router>
+    );
+  }
+}
+
+export default App;
